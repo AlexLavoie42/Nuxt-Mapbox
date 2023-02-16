65e54f09a98764cd0a485637b02168cc9cfd6863
Alex Lavoie <lavoiealex@hotmail.com>
2023-02-15 21:38:23 -0800

    feat: beginning mapbox event implementation

--- src/runtime/components/Map.vue
+++ src/runtime/components/Map.vue
@@ -1,17 +1,51 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-    import { MapboxOptions } from 'mapbox-gl';
+    import mapboxgl, { MapboxOptions, MapDataEvent } from 'mapbox-gl';
     import { provide, onMounted, StyleValue } from 'vue';
-    import { _mapboxInit } from '../composables/mapboxInit';
+    import { defineMapboxInstance } from '../composables/defineMapboxInstance';
     
     type MapboxComponentOptions = Omit<MapboxOptions, "container">
 
     const props = defineProps<{ mapId: string, options?: MapboxComponentOptions }>()
 
+    const emit = defineEmits<{
+      (e: 'load'): void
+      (e: 'render'): void
+      (e: 'idle'): void
+      (e: 'error'): void
+      (e: 'webglcontextlost'): void
+      (e: 'webglcontextrestored'): void
+
+      (e: 'data', event: MapDataEvent): void
+      (e: 'styledata', event: MapDataEvent): void
+      (e: 'sourcedata', event: MapDataEvent): void
+      (e: 'dataloading', event: MapDataEvent): void
+      (e: 'styledataloading', event: MapDataEvent): void
+      (e: 'sourcedataloading', event: MapDataEvent): void
+      (e: 'styleimagemissing', event: MapDataEvent): void
+    }>()
+
     provide('MapID', props.mapId)
     onMounted(() => {
-        _mapboxInit(props.mapId, {...props.options, container: props.mapId})
+      const map = defineMapboxInstance(props.mapId, {...props.options, container: props.mapId});
+
+      map.value?.on('load', () => { emit("load") })
+      map.value?.on('render', () => { emit("render") })
+      map.value?.on('idle', () => { emit("idle") })
+      map.value?.on('error', () => { emit("error") })
+      map.value?.on('webglcontextlost', () => { emit("webglcontextlost") })
+      map.value?.on('webglcontextrestored', () => { emit("webglcontextrestored") })
+
+      map.value?.on('data', (e) => { emit("data", e) })
+      map.value?.on('styledata', (e) => { emit("styledata", e) })
+      map.value?.on('sourcedata', (e) => { emit("sourcedata", e) })
+      map.value?.on('dataloading', (e) => { emit("dataloading", e) })
+      map.value?.on('styledataloading', (e) => { emit("styledataloading", e) })
+      map.value?.on('sourcedataloading', (e) => { emit("sourcedataloading", e) })
+      map.value?.on('styleimagemissing', (e) => { emit("styleimagemissing", e) })
     })
+
+    
 </script>
 
 
