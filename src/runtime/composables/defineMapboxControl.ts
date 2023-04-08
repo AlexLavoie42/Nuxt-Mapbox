748d9f982f6949bdfc4e5aa66f9a486898494235
Alex Lavoie <lavoiealex@hotmail.com>
2023-04-08 15:48:03 -0700

    fix: custom control improvement

--- src/runtime/composables/defineMapboxControl.ts
+++ src/runtime/composables/defineMapboxControl.ts
@@ -1,7 +1,18 @@
-import { Map, Control } from 'mapbox-gl';
+import { Map, IControl } from 'mapbox-gl';
 
-export function defineMapboxControl(onAdd: (map: Map) => HTMLElement, onRemove: (map: Map) => void): Control {
-    const control = new Control();
+export function defineMapboxControl(onAdd: (map: Map) => HTMLElement, onRemove: (map: Map) => void): IControl {
+    class CustomControl implements IControl {
+        constructor() {}
+        onAdd(map: Map): HTMLElement {
+            const html = onAdd(map);
+            html.remove();
+            return html;
+        }
+        onRemove(map: Map): void {
+            onRemove(map);
+        }
+    }
+    const control = new CustomControl();
 
     control.onAdd = onAdd;
     control.onRemove = onRemove;
