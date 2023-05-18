85b87e11e50d27f6b534dcc012222effc89c35df
Alex Lavoie <lavoiealex@hotmail.com>
2023-05-17 18:36:02 -0700

    types: type fixes

--- src/module.ts
+++ src/module.ts
@@ -1,16 +1,20 @@
 import { defineNuxtModule, addPlugin, createResolver, addImportsDir, addComponentsDir } from '@nuxt/kit'
-import { AppConfig } from 'vue'
+import { AppConfig } from '@nuxt/schema'
 // Module options TypeScript inteface definition
-export interface ModuleOptions {
+export interface MapboxOptions {
   accessToken: string
   baseApiUrl?: string
   workerUrl?: string
   workerCount?: number
   prewarm?: boolean,
+}
+export interface InternalOptions {
   persistent?: boolean
 }
 
-export type ExtendedAppConfig = AppConfig & { _MAPBOX_CONFIG: ModuleOptions }
+type ModuleOptions = MapboxOptions & InternalOptions;
+
+export type ExtendedAppConfig = AppConfig & { _MAPBOX_CONFIG: MapboxOptions }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -31,8 +35,14 @@ export default defineNuxtModule<ModuleOptions>({
     nuxt.options.app.head.script?.push({ src: 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js' })
     if (options.persistent) nuxt.options.app.keepalive = true;
     
-    const appConfig = nuxt.options.appConfig
-    appConfig._MAPBOX_CONFIG = options;
+    const appConfig = nuxt.options.appConfig as ExtendedAppConfig
+    appConfig._MAPBOX_CONFIG = {
+      accessToken: options.accessToken,
+      baseApiUrl: options.baseApiUrl,
+      workerUrl: options.workerUrl,
+      workerCount: options.workerCount,
+      prewarm: options.prewarm
+    }
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin.client'))
