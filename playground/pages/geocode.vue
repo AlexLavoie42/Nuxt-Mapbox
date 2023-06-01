ebac8f9d232b3b62c2c1e54712c645599c6a49dd
Alex Lavoie <alexflavoie@gmail.com>
2023-05-31 22:28:45 -0700

    chore: playground testing

--- playground/pages/geocode.vue
+++ playground/pages/geocode.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { ref, watch } from '#imports';
 
+const result = ref<MapboxGeocoder.Result>();
+watch(result, (res) => {
+  console.log(res?.place_name);
+})
 </script>
 
 <template>
   <div>
     <MapboxCustomGeocoder
+      v-model="result"
       @change="console.log('change')"
       @keydown="console.log('keydown')"
     >
