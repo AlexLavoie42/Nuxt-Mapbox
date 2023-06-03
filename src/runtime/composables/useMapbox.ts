42bf373150dad578b887fd16cc5d5b71fcb715b2
Alex Lavoie <alexflavoie@gmail.com>
2023-06-02 17:54:30 -0700

    fix: using isStyleLoaded

--- src/runtime/composables/useMapbox.ts
+++ src/runtime/composables/useMapbox.ts
@@ -6,12 +6,12 @@ type MapboxCallback = (map: Map) => void
 
 export function useMapbox(mapID: string, callback: MapboxCallback): void {
     const map = useMapboxInstance(mapID);
-    if (map.value && map.value.loaded()) return callback(map.value);
+    if (map.value && map.value.isStyleLoaded()) return callback(map.value);
     else if (map.value) map.value.on('load', () => callback(map.value as Map));
 
     watch(map, () => {
         if (map.value) {
-            if (map.value.loaded()) return callback(map.value);
+            if (map.value.isStyleLoaded()) return callback(map.value);
 
             map.value.on('load', () => { callback(map.value as Map) });
         }
