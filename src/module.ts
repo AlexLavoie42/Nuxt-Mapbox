0e1fcb6e74a7cc50ef5054b4cf2a5a503394ff06
Alex Lavoie <alexflavoie@gmail.com>
2023-07-19 12:22:48 -0700

    fix: mapbox alias test

--- src/module.ts
+++ src/module.ts
@@ -36,6 +36,8 @@ export default defineNuxtModule<ModuleOptions>({
     // nuxt.options.app.head.script?.push({ src: 'https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.js' })
     // nuxt.options.app.head.script?.push({ src: 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js' })
     if (options.persistent) nuxt.options.app.keepalive = true;
+
+    nuxt.options.alias['mapbox-gl'] = 'mapbox-gl'
     
     const appConfig = nuxt.options.appConfig as ExtendedAppConfig
     appConfig._MAPBOX_CONFIG = {
