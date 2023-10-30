33df66ac50af95d33ed37c7882998230fb29dabb
Alex Lavoie <alexflavoie@gmail.com>
2023-10-30 11:45:24 -0700

    finishing basic docs structure

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus'
+  extends: '@nuxt-themes/docus',
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        verbatimModuleSyntax: false
+      }
+    }
+  }
 })
