6ca0639bf4d935f2bf99bc042acc8056bbe424dd
Alex Lavoie <alexflavoie@gmail.com>
2023-10-30 11:58:28 -0700

    small docs fixes

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,17 +1,8 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
+    title: 'Nuxt Mapbox',
+    description: 'Elegant Mapbox GL JS integration with Nuxt',
+    image: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/mapbox_logo_icon_169974.png',
     github: {
       dir: '.starters/default/content',
       branch: 'main',
