bf3d44f00c86f59e6fbb5bd7e698cf89c7a2c82f
Alex Lavoie <alexflavoie@gmail.com>
2023-10-30 15:42:11 -0700

    chore: docs fix 3

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: ['@nuxt-themes/docus'],
   typescript: {
     tsConfig: {
       compilerOptions: {
