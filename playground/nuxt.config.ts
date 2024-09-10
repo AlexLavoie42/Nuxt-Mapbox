963e0f43abbc8eab0fda5ff60635729fe8d613bf
Alex Lavoie <alex@properate.io>
2024-09-10 10:07:01 -0700

    package updates

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: false,
+
   mapbox: {
     accessToken: 'pk.eyJ1IjoiYWxleGxhdm9pZSIsImEiOiJja3RhZmpqZmoxbGlzMndvOGtueDFxYTU3In0.nyGbO7RFkmwC_kYseOZ4Dw'
-  }
+  },
+
+  compatibilityDate: '2024-09-10'
 })
